fdf38092ec23476ea40257e7cfff33e10e41dbb1
TaeJin <ktj503@naver.com>
2024-08-19 07:32:05 +0900

    [Silver III] Title: 통계학, Time: 368 ms, Memory: 52732 KB -BaekjoonHub

--- Python/백준/Silver/2108. 통계학/통계학.py
+++ Python/백준/Silver/2108. 통계학/통계학.py
@@ -1,4 +1,6 @@
 import sys
+from collections import Counter
+
 n = int(sys.stdin.readline())
 num_list = []
 for i in range(n):
@@ -10,29 +12,13 @@ num_list.sort()
 
 print(num_list[n // 2])
 
-count = 1
-big_count = 1
-mode_list = []
-for i in range(1,n):
-    if num_list[i] == num_list[i-1]:
-        count +=1
-    else:
-        if count > big_count:
-            big_count = count
-            mode_list = []
-            mode_list.append(num_list[i-1])
-        elif count == big_count:
-            mode_list.append(num_list[i-1])
-        count = 1
-        
-if count > big_count:
-    mode_list = [num_list[-1]]
-elif count == big_count:
-    mode_list.append(num_list[-1])
-        
-if len(mode_list) == 1:
-    print(mode_list[0])
+
+counter = Counter(num_list)
+mode_freq = counter.most_common()
+if len(mode_freq) > 1 and mode_freq[0][1] == mode_freq[1][1]:
+    mode = mode_freq[1][0]
 else:
-    print(mode_list[1])
+    mode = mode_freq[0][0]
+print(mode)
 
 print(num_list[-1]-num_list[0])
