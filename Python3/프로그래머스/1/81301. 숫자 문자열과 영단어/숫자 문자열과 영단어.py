21cc96035a4cbf8a21c621ca972da042859fb06f
TaeJin <ktj503@naver.com>
2024-09-21 18:06:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,21 @@
+def solution(s):
+    num_list = ['zero','one','two','three','four','five','six','seven', 'eight', 'nine']
+    
+    temp = []
+    ans = []
+    
+    48-57
+    for i in range(len(s)):
+        if 48<=ord(s[i])<=57:
+            ans.append(s[i])
+        else:
+            temp.append(s[i])
+            
+        temp_str = ''.join(temp)
+        if temp_str in num_list:
+            ans.append(str(num_list.index(temp_str)))
+            temp = []
+    return int(''.join(ans))
+            
+        
+        
