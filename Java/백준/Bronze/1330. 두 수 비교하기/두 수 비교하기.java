7a03eba0b4122054ec54b6fb25ca9251f64f65e0
TAEJIN KIM <ktj503@naver.com>
2024-12-28 06:31:12 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- Java/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ Java/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -5,7 +5,7 @@ public class Main{
     public static void main(String args[]) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line = br.readLine();
-        StringTokenizer st = new StringTokenizer(line, " ");
+        StringTokenizer st = new StringTokenizer(line);
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         
