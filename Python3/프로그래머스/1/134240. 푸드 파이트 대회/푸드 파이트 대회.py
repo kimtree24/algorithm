d29301ad55f37ca6e4aa4c530a3deb7c78b32054
TAEJIN KIM <ktj503@naver.com>
2025-09-21 13:43:17 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- Python3/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ Python3/프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,15 +1,10 @@
 def solution(food):
-    result = ""
+    temp_result = []
     
     # 0 기준 전단
     for i in range(1,len(food)):
         use_food_num = food[i] // 2 # 사용할 수 있는 음식 수 (전단)
-        for j in range(use_food_num):
-            result += str(i)
-            
-    result += "0"
+        temp_result.append(str(i) * use_food_num)
     
-    #  0 기준 후단
-    for i in result[-2::-1]:
-        result += i
-    return(result)
+    left_str = ''.join(temp_result)
+    return left_str + '0' + left_str[::-1]
