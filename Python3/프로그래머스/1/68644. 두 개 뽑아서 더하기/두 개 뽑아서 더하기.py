05f70cf1931c1d0598c510bd8c13f38ef21eee8f
TAEJIN KIM <ktj503@naver.com>
2025-09-21 12:59:05 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.29 ms, Memory: 9.57 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+def solution(numbers):
+    combi_set = set(list(combinations(numbers, 2)))
+    result = set()
+    for each_combi in combi_set:
+        result.add(sum(each_combi))
+    
+    return sorted(list(result))
+        
+    
