9eba50e160174d13938931d8432f6d707554dac4
TAEJIN KIM <ktj503@naver.com>
2025-09-21 17:45:56 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,16 +1,6 @@
 def solution(strings, n):
-    new_strings = [] # n번째 char 붙여서 새로 만든 문자열
-    result = [] # 문자열 원복 저장용
+    return sorted(strings, key = lambda x: (x[n], x))
     
-    for each_str in strings:
-        new_strings.append(each_str[n] + each_str) # 판단 문자열 생성
-    
-    new_strings.sort()
-    
-    # 원복
-    for i in new_strings:
-        result.append(i[1:])
-    return result
     
         
         
