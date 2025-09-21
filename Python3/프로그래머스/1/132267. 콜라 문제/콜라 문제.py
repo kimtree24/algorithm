d0d659ac80ba87f5cc7a530f66ae7a6c4e6a94be
TAEJIN KIM <ktj503@naver.com>
2025-09-21 17:24:18 +0900

    [level 1] Title: 콜라 문제, Time: 0.71 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,13 @@
+def solution(a, b, n):
+    
+    cur_bot = n # 지금 가지고 있는 빈 병
+    
+    result = 0 # 교환해서 받은 빈 병
+    
+    # 지금 가지고 있는 빈 병이 교환 가능한 최소 개수보다 큰 경우 계속 실행
+    while cur_bot >= a:
+        new_bot = (cur_bot // a) * b # 이번 교환으로 새로 받을 수 있는 콜라
+        result += new_bot # 새로 받은 만큼 add
+        cur_bot = cur_bot - ((new_bot / b) * a) + new_bot # 지금 가진 병 갱신
+    
+    return result
