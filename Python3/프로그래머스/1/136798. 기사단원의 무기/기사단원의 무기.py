c0ef2165a33a25b9b2e5c54f52b1ca3772f671dc
TAEJIN KIM <ktj503@naver.com>
2025-09-22 09:05:39 +0900

    [level 1] Title: 기사단원의 무기, Time: 120.47 ms, Memory: 9.88 MB -BaekjoonHub

--- Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,23 +1,13 @@
-import math
 def solution(number, limit, power):
-    
+    div_cnt = [0] * (number+1) # 각 정수에 대한 약수 카운트 배열
+    for i in range(1, number+1): # i를 모든 배수 j의 약수로 기록
+        for j in range(i, number+1, i):
+            div_cnt[j] += 1
+
     result = 0
-    
-    for num_knight in range(1,number+1):
-        # 지금 해당 기사의 약수 개수(가질 수 있는 공격력)
-        sqrt_num = int(math.sqrt(num_knight))
-        att_num = 0
-        for j in range(1,sqrt_num+1):
-            if num_knight % j == 0:
-                att_num += 1
-        att_num *= 2
-        if math.sqrt(num_knight) == sqrt_num:
-            att_num -= 1
-        
-        # 구매하는 무기 제한
-        if att_num > limit:
-            att_num = power
-        
-        result += att_num
+    for i in range(1, number+1):
+        att = div_cnt[i] # i의 약수 개수
+        if att > limit:
+            att = power
+        result += att
     return result
-        
