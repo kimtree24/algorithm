6ca78774cf7b9c0a5301b050cf30b29d56f58f82
TAEJIN KIM <ktj503@naver.com>
2025-09-24 08:37:57 +0900

    [level 1] Title: 모의고사, Time: 3.29 ms, Memory: 9.36 MB -BaekjoonHub

--- Python3/프로그래머스/1/42840. 모의고사/모의고사.py
+++ Python3/프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,40 +2,17 @@ def solution(answers):
     _1 = [1,2,3,4,5]
     _2 = [2,1,2,3,2,4,2,5]
     _3 = [3,3,1,1,2,2,4,4,5,5]
-    
-    idx_1, idx_2, idx_3 = 0,0,0
+
     ans_num = {1:0, 2:0, 3:0}
-    
-    for i in range(len(answers)):
-        if answers[i] == _1[idx_1]:
+
+    for i, ans in enumerate(answers):
+        if ans == _1[i % len(_1)]:
             ans_num[1] += 1
-        if idx_1 < 4:
-            idx_1 += 1
-        else:
-            idx_1 = 0
-                
-        if answers[i] == _2[idx_2]:
+        if ans == _2[i % len(_2)]:
             ans_num[2] += 1
-        if idx_2 < 7:
-            idx_2 += 1
-        else:
-            idx_2 = 0
-            
-        if answers[i] == _3[idx_3]:
+        if ans == _3[i % len(_3)]:
             ans_num[3] += 1
-        if idx_3 < 9:
-            idx_3 += 1
-        else:
-            idx_3 = 0
-    
-    max_key = max(ans_num, key = ans_num.get)
-    result = []
-    if ans_num[max_key] == ans_num[1]:
-        result.append(1)
-    if ans_num[max_key] == ans_num[2]:
-        result.append(2)
-    if ans_num[max_key] == ans_num[3]:
-        result.append(3)
-    return result
-    
-        
+
+    max_value = max(ans_num.values())
+
+    return [k for k, v in ans_num.items() if v == max_value]
