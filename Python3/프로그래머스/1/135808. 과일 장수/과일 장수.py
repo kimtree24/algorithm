d719881a3665b796546d4953c90a33c31d7d6376
TAEJIN KIM <ktj503@naver.com>
2025-09-25 08:46:29 +0900

    [level 1] Title: 과일 장수, Time: 107.48 ms, Memory: 28.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ Python3/프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,14 +1,8 @@
 def solution(k, m, score):
-    num_box = len(score) // m
-    
-    score_sorted = sorted(score, reverse = True)
-    
-    idx = 0
-    
+    score_sorted = sorted(score, reverse=True)
     result_sum = 0
-    
-    for i in range(num_box):
-        this_box = score_sorted[idx:idx+m]
-        result_sum += this_box[-1]
-        idx += m
+
+    for i in range(m-1, len(score), m):
+        result_sum += score_sorted[i]
+
     return result_sum * m
