30aefab66413efa86eb79948d505a1a7b7a44e9f
TAEJIN KIM <ktj503@naver.com>
2025-10-01 09:21:42 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.05 ms, Memory: 9.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/160586. 대충 만든 자판/README.md
+++ Python3/프로그래머스/1/160586. 대충 만든 자판/README.md
@@ -1,10 +1,10 @@
 # [level 1] 대충 만든 자판 - 160586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/160586#) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.80 ms
+메모리: 9.2 MB, 시간: 1.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 09:06:49
+2025년 10월 01일 09:21:40
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,22 +1,22 @@
 def solution(keymap, targets):
-    key_map = {'A':-1, 'B': -1,'C':-1, 'D': -1,'E':-1, 'F': -1,'G':-1, 'H': -1,'I':-1, 'J': -1,'K':-1, 'L': -1,'M':-1, 'N': -1,'O':-1, 'P': -1,'Q':-1, 'R': -1,'S':-1, 'T': -1,'U':-1, 'V': -1,'W':-1, 'X': -1,'Y':-1, 'Z': -1}
-    for each_key in keymap:
-        for i, each_char in enumerate(each_key):
-            if key_map[each_char] != -1 and i+1 < key_map[each_char]:
-                key_map[each_char] = i+1
-            elif key_map[each_char] == -1:
-                key_map[each_char] = i+1
-    result = []
-    for each_target in targets:
-        each_sum = 0
-        for each_char in each_target:
-            if key_map[each_char] == -1:
-                each_sum = -1
+    INF = 10**9
+    key_map = [INF] * 26  # A~Z
+
+    for row in keymap:
+        for i, ch in enumerate(row):
+            idx = ord(ch) - 65
+            if i + 1 < key_map[idx]:
+                key_map[idx] = i + 1
+
+    ans = []
+    for word in targets:
+        s = 0
+        ok = True
+        for ch in word:
+            idx = ord(ch) - 65
+            if not (0 <= idx < 26) or key_map[idx] == INF:
+                ok = False
                 break
-            else:
-                each_sum += key_map[each_char]
-        result.append(each_sum)
-    return result
-        
-        
-    
+            s += key_map[idx]
+        ans.append(s if ok else -1)
+    return ans
