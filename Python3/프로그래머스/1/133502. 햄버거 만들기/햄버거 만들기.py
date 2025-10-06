50040b2437e1c2cc2849359675a664801fdf38cf
TAEJIN KIM <ktj503@naver.com>
2025-10-06 20:40:24 +0900

    [level 1] Title: 햄버거 만들기, Time: 142.82 ms, Memory: 25.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,19 @@
+def solution(ingredient):
+    ans = 0
+    cur_ing = []
+    last_ing = 0
+    for i in ingredient:
+        cur_ing.append(i)
+        if i == 1:
+            if last_ing == 3 and cur_ing[-4:] == [1,2,3,1]:
+                ans += 1
+                cur_ing.pop()
+                cur_ing.pop()
+                cur_ing.pop()
+                cur_ing.pop()
+            last_ing = 1
+        elif i == 2:
+            last_ing = 2
+        elif i == 3:
+            last_ing = 3
+    return ans
