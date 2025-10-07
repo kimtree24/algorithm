1caadae688c43a9282fb34f923185d9fed740f07
TAEJIN KIM <ktj503@naver.com>
2025-10-07 16:06:36 +0900

    [level 1] Title: 숫자 짝꿍, Time: 760.31 ms, Memory: 29.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,15 +1,23 @@
-from collections import Counter
-
 def solution(X, Y):
-    cnt_X = Counter(X)
-    cnt_Y = Counter(Y)
-    cnt_XY = cnt_X & cnt_Y
-    result = "".join(sorted(list(cnt_XY.elements()), reverse = True))
-    if result == "":
+    cnt_X = [0] * 10
+    cnt_Y = [0] * 10
+    
+    for ch in X:
+        cnt_X[ord(ch) - 48] += 1
+    for ch in Y:
+        cnt_Y[ord(ch) - 48] += 1
+    
+    result = []
+    for i in range(9, -1, -1):
+        k = min(cnt_X[i], cnt_Y[i])
+        if k > 0:
+            result.append(str(i) * k)
+    result_str = "".join(result)
+    
+    if not result:
         return "-1"
-    elif result.startswith("0"):
+    elif result_str[0] == "0":
         return "0"
     else:
-        return result
-    
-    
+        return result_str
+            
