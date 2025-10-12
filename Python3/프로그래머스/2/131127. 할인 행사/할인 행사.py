6161b745a4c1bb4731fb5c9e46ee713d772d3e70
TAEJIN KIM <ktj503@naver.com>
2025-10-13 08:41:24 +0900

    [level 2] Title: 할인 행사, Time: 110.85 ms, Memory: 13.7 MB -BaekjoonHub

--- Python3/프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ Python3/프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,25 +1,30 @@
+from collections import Counter
+
 def solution(want, number, discount):
+    need = {item: cnt for item, cnt in zip(want, number)}
+    n = len(discount)
+    W = 10
     result = 0
-    last_idx = 9
-    while last_idx < len(discount):
-        # 필요 물품 수량 체크
-        stuff_dict = {stuff: number[idx] for idx, stuff in enumerate(want)}
-        
-        flag = True
-        
-        # 매번 10개 판단하기 위함
-        start_idx = last_idx - 9
-        
-        parsed_list = discount[start_idx: last_idx + 1]
-        for each_discount in parsed_list:
-            if each_discount in want and stuff_dict[each_discount] > 0:
-                stuff_dict[each_discount] -= 1
-            else:
-                flag = False
-                break
-        
-        if flag:
+    
+    # 판단할 리스트 초기화
+    window = Counter(discount[:W])
+
+    if all(window.get(k, 0) == need[k] for k in need):
+        result += 1
+
+    # 슬라이딩
+    for i in range(W, n):
+        out_item = discount[i - W]
+        in_item  = discount[i]
+
+        # 윈도우 갱신
+        window[out_item] -= 1
+        if window[out_item] == 0:
+            del window[out_item]
+        window[in_item] = window.get(in_item, 0) + 1
+
+        # 필요 수량과 정확히 일치하는지 검사
+        if all(window.get(k, 0) == need[k] for k in need):
             result += 1
-        
-        last_idx += 1
+
     return result
