27e5cb2b8a06f734dd89154cec9966cdc209f1c6
TAEJIN KIM <ktj503@naver.com>
2025-10-14 08:54:03 +0900

    [level 2] Title: H-Index, Time: 0.15 ms, Memory: 9.17 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,21 +1,9 @@
 def solution(citations):
-    n = len(citations)
-    sorted_citations = sorted(citations, reverse = True)
+    citations.sort(reverse=True)
     h = 0
-    idx = 0
-    while idx < n:
-        # 이번에 검증할 것
-        each_citation = sorted_citations[idx]
-        temp_h = 0
-        
-        for citation in sorted_citations:
-            if each_citation <= citation:
-                temp_h += 1
-            else:
-                break
-        
-        if temp_h <= each_citation:
-            h = temp_h
-        idx += 1
+    for i, c in enumerate(citations):
+        if c >= i + 1:
+            h = i + 1
+        else:
+            break
     return h
-        
