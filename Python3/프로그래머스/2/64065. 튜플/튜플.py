93551f307304032ded810750b7506cb79a5f8467
TAEJIN KIM <ktj503@naver.com>
2025-10-17 08:56:36 +0900

    [level 2] Title: 튜플, Time: 50.09 ms, Memory: 19 MB -BaekjoonHub

--- Python3/프로그래머스/2/64065. 튜플/튜플.py
+++ Python3/프로그래머스/2/64065. 튜플/튜플.py
@@ -1,28 +1,7 @@
+import re
+from collections import Counter
+
 def solution(s):
-    s = s[1:len(s)-1]
-    s_list = []
-    temp_word = []
-    for ch in s:
-        if ch == '}':
-            temp_word.append(ch)
-            each_set = ''.join(temp_word)
-            s_list.append(each_set)
-        elif ch == "{":
-            temp_word = ["{"]
-        else:
-            temp_word.append(ch)
-    sorted_s = sorted(s_list, key = len)
-    
-    result = []
-    
-    for each_set in sorted_s:
-        each_set = each_set[1: len(each_set) - 1]
-        each_num_list = each_set.split(',')
-        
-        for each_num in each_num_list:
-            int_each_num = int(each_num)
-            if int(each_num) not in result:
-                result.append(int_each_num)
-    return result
-        
-    
+    nums = re.findall(r'\d+', s)
+    cnt = Counter(map(int, nums))
+    return [num for num, _ in cnt.most_common()]
