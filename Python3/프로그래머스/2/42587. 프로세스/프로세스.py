1fd207a2092e9ed16a3ac874e46edec56bfea8c1
TAEJIN KIM <ktj503@naver.com>
2025-10-20 08:48:56 +0900

    [level 2] Title: 프로세스, Time: 0.08 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/2/42587. 프로세스/프로세스.py
+++ Python3/프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,33 +1,15 @@
 from collections import deque
 
 def solution(priorities, location):
-    max_priority = max(priorities)
-    
-    priorities_q = deque(priorities)
-    cnt = 0
-    
-    while priorities_q:
-        process = priorities_q[0]
-        # 실행
-        if max_priority <= process:
-            priorities_q.popleft()
-            cnt += 1
-            # 지금 찾고자 하는 값이라면
-            if location == 0:
-                return cnt
-            # 실행시키면 location -1
-            location -= 1
-            # 최고 우선순위 갱신
-            max_priority = max(priorities_q)
-        # 후순위로
+    q = deque([(p, i) for i, p in enumerate(priorities)])
+    sorted_priorities = sorted(priorities, reverse=True)
+    order = 0
+
+    while q:
+        cur = q.popleft()
+        if cur[0] == sorted_priorities[order]:
+            order += 1
+            if cur[1] == location:
+                return order
         else:
-            priorities_q.popleft()
-            priorities_q.append(process)
-            # 지금 찾고자 하는 값이라면
-            if location == 0:
-                location += len(priorities_q) - 1
-            # 후순위로 보내면 location -1
-            else:
-                location -= 1
-    return 0
-        
+            q.append(cur)
