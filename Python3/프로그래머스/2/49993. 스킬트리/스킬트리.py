3f33d185ff3914cd43c01e2cfd58584c9b8c2ef1
TAEJIN KIM <ktj503@naver.com>
2025-10-25 16:14:28 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 9.14 MB -BaekjoonHub

--- Python3/프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ Python3/프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,18 +1,8 @@
-from collections import deque
 def solution(skill, skill_trees):
+    skill_set = set(skill)
     cnt = 0
-    for skill_tree in skill_trees:
-        idx = 0
-        skill_q = deque(skill)
-        while idx < len(skill_tree):
-            in_skill = skill_tree[idx] in skill_q
-            if in_skill and skill_tree[idx] == skill_q[0]:
-                idx += 1
-                skill_q.popleft()
-            elif not in_skill:
-                idx += 1
-            else:
-                break
-        if idx == len(skill_tree):
+    for tree in skill_trees:
+        filtered = ''.join(ch for ch in tree if ch in skill_set)
+        if skill.startswith(filtered):
             cnt += 1
     return cnt
