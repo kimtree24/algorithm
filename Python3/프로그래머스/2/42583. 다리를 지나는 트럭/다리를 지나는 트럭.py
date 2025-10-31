737235bf7e02659682641d1eb8b919a58b614279
TAEJIN KIM <ktj503@naver.com>
2025-10-31 21:50:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 60.04 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ Python3/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,19 +1,32 @@
 from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    t = 0
-    waiting = deque(truck_weights)
-    on_bridge = deque()
-    curr_weight = 0
-
-    while waiting or on_bridge:
-        t += 1
-        if on_bridge and on_bridge[0][1] == t:
-            w, _ = on_bridge.popleft()
-            curr_weight -= w
-        if waiting and curr_weight + waiting[0] <= weight and len(on_bridge) < bridge_length:
-            w = waiting.popleft()
-            curr_weight += w
-            on_bridge.append((w, t + bridge_length))
-
-    return t
+    cnt = 0
+    truck_num = len(truck_weights)
+    total_weight = 0
+    delay_list = deque(truck_weights)
+    on_bridge = deque([0 for _ in range(bridge_length)])
+    clear = 0
+    
+    while clear < truck_num:
+        # 끝나는 트럭
+        out_truck = on_bridge.popleft()
+        if out_truck != 0:
+            clear += 1
+            total_weight -= out_truck
+        # 더 올라 갈 수 있는지 판단 -> 더 못올라감
+        next_truck = 0
+        if delay_list:
+            next_truck = delay_list[0]
+        if total_weight + next_truck > weight:
+            cnt += 1
+            on_bridge.append(0)
+        # 더 올라가기 가능
+        else:
+            cnt += 1
+            new_truck = 0
+            if delay_list:
+                new_truck = delay_list.popleft()
+            on_bridge.append(new_truck)
+            total_weight += new_truck
+        
+    return cnt
