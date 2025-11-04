1e56a946d8a4f9dabb36056f679a310000ffbd2e
TAEJIN KIM <ktj503@naver.com>
2025-11-04 19:28:50 +0900

    [level 2] Title: 호텔 대실, Time: 2.38 ms, Memory: 9.83 MB -BaekjoonHub

--- Python3/프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ Python3/프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,36 +1,19 @@
-from collections import deque
+def to_min(t):
+    h, m = map(int, t.split(':'))
+    return h * 60 + m
 
 def solution(book_time):
-    # 방 q로 관리
-    q_room = []
-    # q의 최대 길이 트래킹
-    q_max = 0
+    events = []
+    for s, e in book_time:
+        start = to_min(s)
+        end = to_min(e) + 10
+        events.append((start, +1))
+        events.append((end, -1))
+    events.sort(key = lambda x : (x[0], x[1]))
     
-    # 소팅 -> 시작시간 순
-    sorted_book_time = sorted(book_time, key = lambda x: x[0])
-    
-    for idx, time_set in enumerate(sorted_book_time):
-        start_str = time_set[0]
-        end_str = time_set[1]
-        # 시간 문자열 파싱
-        start_h, start_m = map(int,start_str.split(':'))
-        end_h, end_m = map(int,end_str.split(':'))
-        # 시간은 전부 분으로 치환
-        start_to_min = start_h * 60 + start_m
-        end_to_min = end_h * 60 + end_m
-        
-        q_room = [each_time for each_time in q_room if each_time[1] + 10 > start_to_min]
-        
-        # 지금 것 추가
-        q_room.append([start_to_min, end_to_min])
-        # 이번 텀 방 수
-        this_room = len(q_room)
-        
-        # 가장 최대 방 수 트래킹
-        q_max = max(q_max, this_room)
-        
-    return q_max
-        
-        
-    
-    
+    cur = 0
+    ans = 0
+    for _, d in events:
+        cur += d
+        ans = max(ans, cur)
+    return ans
