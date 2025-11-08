ca1f380296019617478b2dca7b0575d580c905b8
TAEJIN KIM <ktj503@naver.com>
2025-11-08 22:25:41 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/6603. 로또/로또.py
+++ Python/백준/Silver/6603. 로또/로또.py
@@ -5,16 +5,14 @@ while True:
     nums = list(map(int, input().split()))
     if nums[0] == 0:
         break
-    K = nums[0]
-    S = nums[1:]
     arr = []
     def dfs(start):
         if len(arr) == 6:
             print(*arr)
             return
-        for i in range(start, K):
-            arr.append(S[i])
+        for i in range(start, nums[0]+1):
+            arr.append(nums[i])
             dfs(i+1)
             arr.pop()
-    dfs(0)
+    dfs(1)
     print()
