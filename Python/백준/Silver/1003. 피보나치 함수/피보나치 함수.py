35d843b5621d77376cb614d70ef2ca41525ac791
TAEJIN KIM <ktj503@naver.com>
2026-01-16 23:51:08 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+zero = [0 for _ in range(41)]
+one = [0 for _ in range(41)]
+
+zero[0], one[0] = 1, 0
+zero[1], one[1] = 0, 1
+
+for i in range(2, 41):
+    zero[i] = zero[i-1] + zero[i-2]
+    one[i] = one[i-1] + one[i-2]
+
+for _ in range(t):
+    n = int(input())
+    print(zero[n], one[n])
