1c36707b5466a62a64ede128923841dada236bca
TAEJIN KIM <ktj503@naver.com>
2026-01-18 18:12:23 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+matrix = [0 for _ in range(n + 1)]
+
+matrix[1] = 1
+if n >= 2:
+    matrix[2] = 3
+    
+for i in range(3, n + 1):
+    matrix[i] = (matrix[i - 1] + 2 * matrix[i - 2]) % 10007
+print(matrix[n])
