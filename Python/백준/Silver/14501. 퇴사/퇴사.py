2e562a52a294cdb947acc92add34a11505682b2b
TAEJIN KIM <ktj503@naver.com>
2026-01-25 14:16:22 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+t_list = []
+p_list = []
+
+for _ in range(n):
+    t,p = map(int,input().strip().split())
+    t_list.append(t)
+    p_list.append(p)
+
+dp = [0 for _ in range(n + 1)]
+
+for i in range(n - 1, -1, -1):
+    if i + t_list[i] <= n:
+        dp[i] = max(dp[i + 1], p_list[i] + dp[i + t_list[i]])
+    else:
+        dp[i] = dp[i + 1]
+print(dp[0])
