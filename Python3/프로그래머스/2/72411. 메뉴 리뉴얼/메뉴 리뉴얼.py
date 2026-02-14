8e34807146871802dd3b83a343acc1f3fe4b07a9
TAEJIN KIM <ktj503@naver.com>
2026-02-14 17:11:34 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 14.81 ms, Memory: 9.32 MB -BaekjoonHub

--- Python3/프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ Python3/프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,30 +1,41 @@
-from itertools import combinations
-from collections import defaultdict
 def solution(orders, course):
     
     ans = []
     
-    for n_course in course:
-        # 각 조합 key
-        each_course = defaultdict(int)
+    for i in range(len(orders)):
+        orders[i] = ''.join(sorted(orders[i]))
+    
+    def make_combi(order, start, path, length, counter):
+        if len(path) == length:
+            key = ''.join(path)
+            if key in counter:
+                counter[key] += 1
+            else:
+                counter[key] = 1
+            return
+        for i in range(start, len(order)):
+            path.append(order[i])
+            make_combi(order, i + 1, path, length, counter)
+            path.pop()
+    
+    for length in course:
+        counter = {}
         
         for order in orders:
-            order = sorted(order)
-            combi = combinations(order, n_course)
-            for e_combi in combi:
-                combi_key = ''.join(e_combi)
-                each_course[combi_key] += 1
+            if len(order) < length:
+                continue
+            make_combi(order, 0, [], length, counter)
         
-        if not each_course:
-            continue
+        max_cnt = 0
         
-        max_cnt = max(each_course.values())
+        if counter:
+            max_cnt = max(counter.values())
         
         if max_cnt < 2:
             continue
         
-        for menu, cnt in each_course.items():
-            if cnt == max_cnt:
-                ans.append(menu)
+        for key in counter:
+            if counter[key] == max_cnt:
+                ans.append(key)
     ans.sort()
     return ans
